188719f50ecdff9fda1e6bc8dba1b64b25c7b96b
Rhoda <rhodachela@gmail.com>
2024-09-19 13:54:30 +0300

    A guessing game for fun!

---
+++ fungame.py
@@ -0,0 +1,31 @@
+import random
+
+def guess_the_number():
+    secret_number = random.randint(10, 20)
+    play_again = True
+
+    while play_again:
+        try:
+            guess = int(input("Guess secret number between 10 and 20: "))
+        except ValueError:
+            print("Enter a valid integer")
+            continue
+
+        match guess:
+            case _ if guess ==secret_number:
+                print("Congratulations, you guessed it right!")
+            case _ if guess > secret_number:
+                print("Ooop!, Your number is too high. Try again!")
+            case _ if guess < secret_number:
+                print ("Your number is too low. Try again!")
+        
+        play_again_input = input("Do you want to play again? (yes or no)").strip().lower()
+        if play_again_input != "yes":
+            play_again = False
+            print("Thanks for playing with us!")
+
+guess_the_number()
+
+
+
+
